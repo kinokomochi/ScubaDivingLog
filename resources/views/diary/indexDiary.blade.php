7fe8f751a0bf35e1791e90d9fba67e555cd9e85f
kinokomochi <toadstomochi@gmail.com>
2020-06-01 14:47:22 +0900

    fixed

--- resources/views/diary/indexDiary.blade.php
+++ resources/views/diary/indexDiary.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 
 @foreach($diary as $val)
-    <a href="{{ route('showDiary', ['id' => $val->id]) }}"><p>{{ $val->day . '-' . $val->title }}</p></a>
+    <a href="{{ route('showDiary', ['id' => $val->id]) }}"><p><img src="{{ asset('storage/images/' . $val->image) }}" width="50" height="50">{{ $val->name . 'さんの日記　'. $val->day . '-' . $val->title }}</p></a>
 @endforeach
 
 {{ $diary->links() }}
